0a76015eeed0bf177a998b7eb0821d8897f6eac5
rizzoirfan <rizzo.irfan@knowledgetouch.com>
2022-09-27 16:54:01 +0700

    setup properties file completed

--- src/main/java/com/rizzodevs/configserver/ConfigServerApplication.java
+++ src/main/java/com/rizzodevs/configserver/ConfigServerApplication.java
@@ -2,12 +2,16 @@ package com.rizzodevs.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 
 }
